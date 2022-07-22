5846f37038ccd9e001d8890de74edd0bd6d46def
Arun-Chikkaraju <190040089ece@gmail.com>
2022-07-22 07:33:12 +0000

    testing

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,5 +6,15 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
+    Select=30;
+
+    # input driving
+    dut.sel.value = Select
+   
+
+    await Timer(2, units='ns')
+
+    assert dut.out.value == out, f"mux result is incorrect: {dut.out.value} != inp30"
+
 
     cocotb.log.info('##### CTB: Develop your test here ########')
