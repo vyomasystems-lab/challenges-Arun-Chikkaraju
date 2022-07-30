769921d1b6dbb52dd61a7df76ddf95a287538e5d
Arun-Chikkaraju <109277202+Arun-Chikkaraju@users.noreply.github.com>
2022-07-30 11:34:14 +0530

    Create seq_detect_1011_correct.v

---
+++ level1_design2/seq_detect_1011_correct.v
@@ -0,0 +1,74 @@
+module seq_detect_1011(seq_seen, inp_bit, reset, clk);
+
+  output seq_seen;
+  input inp_bit;
+  input reset;
+  input clk;
+
+  parameter IDLE = 0,
+            SEQ_1 = 1, 
+            SEQ_10 = 2,
+            SEQ_101 = 3,
+            SEQ_1011 = 4;
+
+  reg [2:0] current_state, next_state;
+
+ 
+  assign seq_seen = current_state == SEQ_1011 ? 1 : 0;
+
+ 
+  always @(posedge clk)
+  begin
+    if(reset)
+    begin
+      current_state <= IDLE;
+    end
+    else
+    begin
+      current_state <= next_state;
+    end
+  end
+
+  
+  always @(posedge clk)
+  begin
+    case(current_state)
+    
+      IDLE:
+      begin
+        if(inp_bit == 1 )
+          next_state = SEQ_1;
+        else
+          next_state = IDLE;
+      end
+      SEQ_1:
+      begin
+        if(inp_bit == 1)
+          next_state = SEQ_1;
+        else
+          next_state = SEQ_10;
+      end
+      SEQ_10:
+      begin
+        if(inp_bit == 1)
+          next_state = SEQ_101;
+        else
+          next_state = IDLE;
+      end
+      SEQ_101:
+      begin
+        if(inp_bit == 1)
+          next_state = SEQ_1011;
+        else
+          next_state = SEQ_10;
+      end
+      SEQ_1011:
+      begin
+        if(inp_bit == 1)
+           next_state = SEQ_1;
+        else
+           next_state = SEQ_10;
+      end
+    endcase
+  end
+endmodule
