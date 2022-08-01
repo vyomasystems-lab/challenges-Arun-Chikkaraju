7c073452a3c5a0266d5567c65836796644def07d
Arun-Chikkaraju <109277202+Arun-Chikkaraju@users.noreply.github.com>
2022-08-01 21:15:31 +0530

    Update test_design3.py

--- level3_design/test_design3.py
+++ level3_design/test_design3.py
@@ -1,25 +1,33 @@
-from cocotb.binary import BinaryValue
 import os
-import sys
-import operator
-from cocotb.result import ReturnValue
+import random
+from pathlib import Path
+from cocotb.result import TestFailure
 
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge
 
-  
 @cocotb.test()
-def project1(dut):
-    clock = Clock(dut.clk, 10, units="us")   
-    cocotb.start_soon(clock.start())
+
+async def func(dut):
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+    dut.reset.value = 0
+    await  FallingEdge(dut.clk)  
     dut.reset.value = 1
-    await FallingEdge(dut.clk)  
+    await  FallingEdge(dut.clk)
     dut.reset.value = 0
     await  FallingEdge(dut.clk)
-    await  FallingEdge(dut.clk)
-  
-    dut.A.value=77
-    dut.B.value=49
-    dut.opcode.value=1
-    dut.log.info("output value is %d and error out  bit is %d"%(int(dut.C),int(dut.ERR_out))) 
-
-
-
+    for opr in range(1,15):
+        for i in range(1):
+            a_value=random.randint(1, 50000)
+            b_value=random.randint(1, 50000)
+            await FallingEdge(dut.clk)
+            dut.A.value=a_value
+            dut.B.value=b_value
+            dut.opcode.value=opr
+            await FallingEdge(dut.clk)
+            out=(a_value%b_value)
+            dut.log.info("Design with Value of A:%s  Value of B:%s out is :%s operation is:%s "%(int(dut.A),int(dut.B),int(dut.C),int(dut.opcode)))
+            await FallingEdge(dut.clk)
+           
